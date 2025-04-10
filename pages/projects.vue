4ba8068b96b9360acf4b661d500fcb781053ea95
ThiagoFragaK <thiagofraga@proton.me>
2025-04-09 21:29:25 -0300

    Refactor: Using nuxts api to storage projects and aboutme pages info

--- pages/projects.vue
+++ pages/projects.vue
@@ -42,16 +42,13 @@
             ExternalLink,
             Github,
         },
-        data: () => ({
-            projects: [
-                {
-                    title: "Clean Architecture E-commerce",
-                    stacks: "Vue3/Nuxt3",
-                    url: "https://nuxt-clean-architecture.vercel.app/",
-                    description: `This is a simple e-commerce application developed to demonstrate a clean architecture in the Frontend. It consumes data from a public API called FakerAPI to generate fictitious information about products and orders. The main goal is to provide a scalable, modular, and easy-to-maintain structure.`,
-                    repository: "https://github.com/ThiagoFragaK/Nuxt-Clean-Architecture",
-                }
-            ]
-        })
+        data() {
+            return {
+                projects: []
+            }
+        },
+        async mounted() {
+            this.projects = await $fetch('/api/projects')
+        }
     }
 </script>
