1473322f3d8a87b9898dbe4cfcc04954b25c176c
ThiagoFragaK <thiagofraga@proton.me>
2025-04-15 17:41:27 -0300

    Add: watcher to watch languages change

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script>
+    import { useLanguageStore } from '~/stores/languagesStore';
+    export default {
+        mounted() {
+            const languageStore = useLanguageStore();
+            watch(
+                () => languageStore.getCurrentLanguage,
+                (newLang) => {
+                    this.$i18n.locale = newLang;
+                },
+                { immediate: true }
+            );
+        },
+    };
+</script>
