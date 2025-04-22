baea6c96178f3fd4b5bf2777f7765fc3f2e71d47
ThiagoFragaK <thiagofraga@proton.me>
2025-04-22 18:15:46 -0300

    Add: Locales files for the languages

---
+++ locales/about-me.js
@@ -0,0 +1,19 @@
+export const Greetings = {
+    "pt": "Olá!",
+    "en": "Hello there!",
+};
+
+export const FirstCollumn = {
+    "pt": "Eu sou Thiago Fraga, um Desenvolvedor Full-Stack e Arquiteto de Software com mais de 6 anos de experiência. Minha especialização está no desenvolvimento de aplicações web robustas e escaláveis usando tecnologias modernas como Laravel, .NET e Vue.js. Com uma sólida formação em Arquitetura de Software e uma paixão por código limpo, foco em construir soluções eficientes que resistam ao teste do tempo.",
+    "en": "I'm Thiago Fraga, a Full-Stack Developer and Software architect with over 6 years of experience. My expertise lies in developing robust and scalable web applications using modern technologies like Laravel, .NET, and Vue.js. With a strong background in Software Architecture and a passion for clean code, I focus on building efficient solutions that stand the test of time.",
+};
+
+export const SecondCollumn = {
+    "pt": "Ao longo da minha carreira, trabalhei em projetos que vão de pequenas startups a sistemas empresariais de grande escala, onde não apenas contribui para o desenvolvimento, mas também desempenhei papéis-chave na refatoração de sistemas legados, melhoria de desempenho e garantia de que as melhores práticas sejam seguidas. Estou sempre em busca da excelência em cada linha de código que escrevo, enfatizando arquitetura escalável, otimização e integração contínua. Sou motivado pela oportunidade de resolver problemas complexos, colaborar com equipes talentosas e entregar software significativo que cause impacto.",
+    "en": "Throughout my career, I've worked on projects ranging from small startups to large-scale enterprise systems, where I not only contributed to the development but also played key roles in refactoring legacy systems, improving performance, and ensuring best practices are followed. I’m always striving for excellence in every line of code I write, emphasizing scalable architecture, optimization, and seamless integration. I'm motivated by the opportunity to solve complex problems, collaborate with talented teams, and deliver meaningful software that makes an impact.",
+};
+
+export const Quote = {
+    "pt": "Em questões de estilo, nade com a corrente; em questões de princípio, fique firme como uma rocha.",
+    "en": "In matters of style, swim with the current; in matters of principle, stand like a rock.",
+};
---
+++ locales/navbar.js
@@ -0,0 +1,9 @@
+export const Aboutme = {
+    "pt": "Sobre mim",
+    "en": "About me",
+};
+
+export const Projects = {
+    "pt": "Projetos",
+    "en": "Projects",
+};
